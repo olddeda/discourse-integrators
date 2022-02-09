29dbae083c12ecf59dc60818a19423f56df5d154
Sergey Safronov <old_deda>
2022-02-09 21:12:53 +0300

    Fix

--- javascripts/discourse/components/showcased-topic-list.js
+++ javascripts/discourse/components/showcased-topic-list.js
@@ -10,7 +10,9 @@ export default Component.extend({
   init() {
     this._super(...arguments);
 
-    if (!this.category) return;
+    if (!this.category) {
+      return
+    };
 
     const filter = {
       filter: "latest",
@@ -34,11 +36,6 @@ export default Component.extend({
   @action
   createTopic() {
     if (this.currentUser) {
-      // This is not ideal - we should not be using __container__ here
-      // We can't inject it properly, because ember doesn't allow injecting controllers into components
-      // We can't `sendAction` up to routes/application createNewTopicViaParams because only clojure actions are allowed
-      // We can't use clojure actions because then an openComposer action would have to be passed to every plugin outlet
-      // The best solution is probably a core appEvent or service which themes could trigger
       Discourse.__container__.lookup("controller:composer").open({
         action: "createTopic",
         draftKey: "createTopic",
--- javascripts/discourse/components/two-topic-list.js
+++ javascripts/discourse/components/two-topic-list.js
@@ -20,34 +20,24 @@ export default Component.extend({
 
   @observes("shouldShow")
   _updateBodyClasses() {
-    const shouldCleanup = this.isDestroying || this.isDestroyed;
-    if (!shouldCleanup && this.shouldShow && settings.show_as_sidebar) {
-      document.body.classList.add("showcased-categories-sidebar");
-    } else {
-      document.body.classList.remove("showcased-categories-sidebar");
-    }
+
   },
 
   get categoriesLoaded() {
     return Category.list().length !== 0;
   },
 
-  get category1() {
-    if (!this.categoriesLoaded) return false;
-    return Category.findById(settings.feed_one_category);
-  },
-
-  get category2() {
-    if (!this.categoriesLoaded) return false;
-    return Category.findById(settings.feed_two_category);
+  get category() {
+    if (!this.categoriesLoaded) {
+      return false;
+    }
+    return Category.findById(settings.category);
   },
 
   @discourseComputed("router.currentRouteName")
   shouldShow(currentRouteName) {
-    let showSidebar =
-      settings.show_as_sidebar && currentRouteName === "discovery.latest";
-    return currentRouteName === `discovery.${defaultHomepage()}` || showSidebar;
+    return currentRouteName === `discovery.${defaultHomepage()}`;
   },
 
-  showTopicLists: and("shouldShow", "category1", "category2")
+  showTopicLists: and("shouldShow", "category", "category")
 });
